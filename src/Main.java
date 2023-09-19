11273c8b3951779db48412a6dabd0090b32aee68
Evgeny Marinichev <evg5743@yandex.ru>
2023-09-19 15:19:21 +0300

    add new method max and safeDiv

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(TestIf.abs(3));
+        System.out.println(TestIf.safeDiv(3, -1));
+        System.out.println(TestIf.max(15, 4));
     }
 }
