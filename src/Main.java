7d065745fdc0ce737a54da5bb0887b3422d5b18d
Evgeny Marinichev <evg5743@yandex.ru>
2023-09-19 15:33:46 +0300

    add new method makeDecision and max3

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println(TestIf.abs(3));
         System.out.println(TestIf.safeDiv(3, -1));
         System.out.println(TestIf.max(15, 4));
+        System.out.println(TestIf.makeDecision(4, 4));
+        System.out.println(TestIf.max3(8, 20, 20));
     }
 }
