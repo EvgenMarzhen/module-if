4874f6e3be85c0f6c695ca5c8262a094f745b272
Evgeny Marinichev <evg5743@yandex.ru>
2023-09-20 11:54:59 +0300

    add new methods

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(TestIf.abs(3));
-        System.out.println(TestIf.safeDiv(3, -1));
-        System.out.println(TestIf.max(15, 4));
-        System.out.println(TestIf.makeDecision(4, 4));
-        System.out.println(TestIf.max3(8, 20, 20));
-        System.out.println(TestIf.sum3(3, -3, 0));
-        System.out.println(TestIf.sum2(-3, 11));
-        System.out.println(TestIf.is35(5));
-        System.out.println(TestIf.magic6(1, 6));
-        System.out.println(TestIf.age(61135));
+//        System.out.println(TestIf.abs(3));
+//        System.out.println(TestIf.safeDiv(3, -1));
+//        System.out.println(TestIf.max(15, 4));
+//        System.out.println(TestIf.makeDecision(4, 4));
+//        System.out.println(TestIf.max3(8, 20, 20));
+//        System.out.println(TestIf.sum3(3, -3, 0));
+//        System.out.println(TestIf.sum2(-3, 11));
+//        System.out.println(TestIf.is35(5));
+//        System.out.println(TestIf.magic6(1, 6));
+//        System.out.println(TestIf.age(61135));
+//        System.out.println(TestIf.day(8));
+        System.out.println(TestIf.printDays("четверг"));
     }
 }
